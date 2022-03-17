c8c961ad4bed7026c5164976d3b65d65453018f7
Jose Pablo Wenzel <devwenzel@gmail.com>
2022-03-17 12:46:59 +0000

    refactor: add error handling to cd

--- src/scripts/typescript/generate-project.sh
+++ src/scripts/typescript/generate-project.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cd "$(dirname "$0")"
-cd ".."
+cd "$(dirname "$0")" || exit 1
+cd ".." || exit 1
 mkdir "$1"
-cd "$1"
+cd "$1" || exit 1
 
 yarn init --yes
 yarn add -D typescript jest @types/jest ts-jest
